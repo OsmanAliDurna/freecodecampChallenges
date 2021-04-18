9fb5b43f89edc1f335f7c4d3e944d5be8331866a
Osman Ali Durna <o_durna@hotmail.com>
2021-04-18 22:05:17 +0300

    freeCode Challenges - 3

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Declare JavaScript Variables"""
+text = """Storing Values with the Assignment Operator"""
 
 print(text.title().replace(" ", "").replace('"', "").replace("?", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
