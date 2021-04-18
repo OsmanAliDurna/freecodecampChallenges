93a790522df0389bee9bc28258531550e7b6c560
Osman Ali Durna <o_durna@hotmail.com>
2021-04-18 21:57:34 +0300

    freeCode Challenges

---
+++ rename.py
@@ -0,0 +1,5 @@
+text = """ Python: Division"""
+
+print("hackerRank" +
+text.title().replace(" ","").replace('"',"").replace("?","").replace("!","").replace("'","").replace(",","").replace(":","") +
+".py")
