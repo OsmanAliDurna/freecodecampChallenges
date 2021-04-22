e88fab6f98be4dd2665bae13db5b228ed59dc763
Osman Ali Durna <o_durna@hotmail.com>
2021-04-22 04:01:58 +0300

    DivideOneNumberByAnotherWithJavascript solved

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Multiply Two Numbers with JavaScript"""
+text = """Divide One Number by Another with JavaScript"""
 
 print(text.title().replace(" ", "").replace('"', "").replace("?", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
