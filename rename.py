5a86e5983a2e9780b4f2abeac2006e140b013773
Osman Ali Durna <o_durna@hotmail.com>
2021-04-24 14:48:03 +0300

    CompoundAssignmentWithAugmentedAddition solved

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Finding a Remainder in JavaScript"""
+text = """Compound Assignment With Augmented Addition"""
 
 print(text.title().replace(" ", "").replace('"', "").replace("?", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
