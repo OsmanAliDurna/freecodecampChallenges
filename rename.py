660d8eb4710a3dee39221147770035a71a6398ae
Osman Ali Durna <o_durna@hotmail.com>
2021-04-25 15:15:29 +0300

    EscapingLiteralQuotesInStrings solved !

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Declare String Variables"""
+text = """Escaping Literal Quotes in Strings"""
 
 print(text.title().replace(" ", "").replace('"', "").replace("?", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
