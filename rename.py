c19e991532cd46e7b50f1e9e128544d3c29af7ce
Osman Ali Durna <o_durna@hotmail.com>
2021-04-27 17:34:00 +0300

    ConcatenatingStringsWithThePlusEqualsOperator done

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Concatenating Strings with Plus Operator"""
+text = """Concatenating Strings with the Plus Equals Operator"""
 
 print(text.title().replace(" ", "").replace('"', "").replace("?", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
