ca09fe33dd81dd9faa2b4663437613d7c36d6d34
Osman Ali Durna <o_durna@hotmail.com>
2021-05-01 00:50:42 +0300

    UseBracketNotationToFindTheLastCharacterInAString!

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Use Bracket Notation to Find the Nth Character in a String"""
+text = """Use Bracket Notation to Find the Last Character in a String"""
 
 print(text.title().replace(" ", "").replace('"', "").replace("?", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
