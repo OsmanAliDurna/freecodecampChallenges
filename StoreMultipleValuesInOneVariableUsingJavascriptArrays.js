0e6a53471827f099e1bb57d8ae3ffa188167e79a
Osman Ali Durna <o_durna@hotmail.com>
2021-05-02 22:57:36 +0300

    StoreMultipleValuesInOneVariableUsingJavascriptArr

---
+++ StoreMultipleValuesInOneVariableUsingJavascriptArrays.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+var myArray = [];
+
+myArray[0] = "";
+myArray[1] = 0;
--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Word Blanks"""
+text = """Store Multiple Values in one Variable using JavaScript Arrays"""
 
 print(text.title().replace(" ", "").replace('"', "").replace("?", "").replace("-", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
