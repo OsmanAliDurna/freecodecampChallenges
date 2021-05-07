b8dacd723b01297408e89788105548dde834e4a4
Osman Ali Durna <o_durna@hotmail.com>
2021-05-07 23:03:58 +0300

    LocalScopeAndFunctions solved

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Global Scope and Functions"""
+text = """Local Scope and Functions"""
 
 print(text.title().replace(" ", "").replace("(", "").replace(")", "").replace('"', "").replace("?", "").replace("-", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
