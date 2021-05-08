b77f51f57fef52fe6576e2329c0d966574209758
Osman Ali Durna <o_durna@hotmail.com>
2021-05-08 17:13:49 +0300

    ReturnAValueFromAFunctionWithReturn solved

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Global vs. Local Scope in Functions"""
+text = """Return a Value from a Function with Return"""
 
 print(text.title().replace(" ", "").replace("(", "").replace(")", "").replace('"', "").replace("?", "").replace(".", "").replace("-", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
