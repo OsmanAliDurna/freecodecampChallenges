76d5da4aa918b9df45da3a9f53149a127fa4c395
Osman Ali Durna <o_durna@hotmail.com>
2021-05-08 17:10:29 +0300

    GlobalVsLocalScopeInFunctions solved

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Local Scope and Functions"""
+text = """Global vs. Local Scope in Functions"""
 
-print(text.title().replace(" ", "").replace("(", "").replace(")", "").replace('"', "").replace("?", "").replace("-", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
+print(text.title().replace(" ", "").replace("(", "").replace(")", "").replace('"', "").replace("?", "").replace(".", "").replace("-", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
