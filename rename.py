ca993eac4db025b9af501aa5d0b8ca71b37600f0
Osman Ali Durna <o_durna@hotmail.com>
2021-05-19 00:30:48 +0300

    ReturningBooleanValuesFromFunctions solved

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Replacing If Else Chains with Switch"""
+text = """Returning Boolean Values from Functions"""
 
 print(text.title().replace(" ", "").replace("(", "").replace(")", "").replace('"', "").replace("?", "").replace(".", "").replace("-", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
