65765ddcb835698751005dd5b2406517dc0e20c5
Osman Ali Durna <o_durna@hotmail.com>
2021-05-19 22:55:33 +0300

    AccessingObjectPropertiesWithDotNotation solved

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Build JavaScript Objects"""
+text = """Accessing Object Properties with Dot Notation"""
 
 print(text.title().replace(" ", "").replace("(", "").replace(")", "").replace('"', "").replace("?", "").replace(".", "").replace("-", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
