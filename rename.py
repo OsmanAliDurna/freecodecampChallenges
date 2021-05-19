b06cd7b816460271f86e6deb189946971876f079
Osman Ali Durna <o_durna@hotmail.com>
2021-05-19 22:53:50 +0300

    BuildJavascriptObjects solved

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Return Early Pattern for Functions"""
+text = """Build JavaScript Objects"""
 
 print(text.title().replace(" ", "").replace("(", "").replace(")", "").replace('"', "").replace("?", "").replace(".", "").replace("-", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
