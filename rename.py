3fabe6b0c3a6c9bc928e9c4f3ad3451d9479adda
Osman Ali Durna <o_durna@hotmail.com>
2021-05-19 22:57:29 +0300

    AccessingObjectPropertiesWithBracketNotation done

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Accessing Object Properties with Dot Notation"""
+text = """Accessing Object Properties with Bracket Notation"""
 
 print(text.title().replace(" ", "").replace("(", "").replace(")", "").replace('"', "").replace("?", "").replace(".", "").replace("-", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
