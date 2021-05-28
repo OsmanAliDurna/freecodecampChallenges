19911f6fa69f00498b2abeed3c1c4430330629e9
Osman Ali Durna <o_durna@hotmail.com>
2021-05-28 03:01:17 +0300

    AddNewPropertiesToAJavascriptObject solved

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-text = """Updating Object Properties"""
+text = """Add New Properties to a JavaScript Object"""
 
 print(text.title().replace(" ", "").replace("(", "").replace(")", "").replace('"', "").replace("?", "").replace(".", "").replace("-", "").replace("!", "").replace("'", "").replace(",", "").replace(":", "") +
       ".js")
